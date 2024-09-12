19866f38e538b5b9628a39b00f3347ff5f803ddd
Nick Efimov <lamoz760@gmail.com>
2024-09-12 11:43:13 +0300

    add pinia fetch films

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-import "./assets/styles/main.css";
-
-import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createApp } from "vue";
 
 import App from "./App.vue";
-import router from "./router";
 
-const app = createApp(App);
+import "./assets/main.css";
 
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+createApp(App).use(createPinia()).mount("#app");
